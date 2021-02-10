b4c93c29d2ec150882a16952962d4cf54d9aeb5e
Lujain Al-shammari <lujain.alshammari@learnet.se>
2021-02-10 22:03:18 +0100

    save

---
+++ main.js
@@ -0,0 +1,9 @@
+let button= document.querySelector('.close_btn')
+let inputValue= document.querySelector('.input1')
+
+let name= document.querySelector('.CityName')
+let name= document.querySelector('.Cityday')
+let name= document.querySelector('.CityTemp')
+let name= document.querySelector('.CityCondition')
+
+fetch()
