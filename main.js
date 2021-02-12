bafb71ed205861f3e6e1cc28f6c0dba99e1963e0
Lujain Al-shammari <lujain.alshammari@learnet.se>
2021-02-12 19:27:09 +0100

    save

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let button= document.querySelector('#close_btn');
 let inputValue= document.querySelector('#inputClass');
 
+
+
 let parent = document.querySelector('.item3');
 button.addEventListener('click', function(){
     
@@ -30,28 +32,21 @@ let condition = document.createElement('p');
 
 })
 
-etch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=ee6dd9a7c626a42fc312a735b559fe70&units=metric")
-.then(response=>response.json())
-//.then(data=> console.log(data))
+//foursqure men i XMLHttpRequest
+const clientId = "J4MCXX0FXC0OB4DOCLMF3CMPUSWK3FXDQLWNPQFRN4Y3SGM3";
+const clientSecret = "KI5OXWD21JUZLOGVHTPSFJRCPQD1YC5XU0BLYK5AAYC1YYVQ";
 
-.then(data=> {
- let nameValaue= data['name'];
- let tempValue= data['main']['temp'];
- let conditionValue= data['weather'][0]['description'];
+const todaysDate = "20210212";
 
-let namn = document.createElement('h2');
-let temp = document.createElement('p');
-let condition = document.createElement('p');
+const venueUrl = new URL("https://api.foursquare.com/v2/venues/explore");
+venueUrl.searchParams.append("client_id", clientId);
+venueUrl.searchParams.append("client_secret", clientSecret);
+venueUrl.searchParams.append("near", inputValue.value);
+venueUrl.searchParams.append("v", todaysDate);
+venueUrl.searchParams.append("limit", 10);
+console.log(venueUrl);
 
- namn.innerHTML = nameValaue;
- temp.innerHTML = tempValue + " °C";
- condition.innerHTML = conditionValue;
+const venueRequest = new XMLHttpRequest();
+venueRequest.open("GET", venueUrl);
+venueRequest.responseType = "json";
 
- parent.appendChild(namn);
- parent.appendChild(temp);
- parent.appendChild(condition);
-})
-
-.catch(err=> alert("No City found!"))
-
-})
