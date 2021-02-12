45a89850e6bb01eecbe02840501226330a7c14e7
Lujain Al-shammari <lujain.alshammari@learnet.se>
2021-02-12 12:41:00 +0100

    save

--- main.js
+++ main.js
@@ -29,3 +29,29 @@ let condition = document.createElement('p');
 .catch(err=> alert("No City found!"))
 
 })
+
+etch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=ee6dd9a7c626a42fc312a735b559fe70&units=metric")
+.then(response=>response.json())
+//.then(data=> console.log(data))
+
+.then(data=> {
+ let nameValaue= data['name'];
+ let tempValue= data['main']['temp'];
+ let conditionValue= data['weather'][0]['description'];
+
+let namn = document.createElement('h2');
+let temp = document.createElement('p');
+let condition = document.createElement('p');
+
+ namn.innerHTML = nameValaue;
+ temp.innerHTML = tempValue + " °C";
+ condition.innerHTML = conditionValue;
+
+ parent.appendChild(namn);
+ parent.appendChild(temp);
+ parent.appendChild(condition);
+})
+
+.catch(err=> alert("No City found!"))
+
+})
